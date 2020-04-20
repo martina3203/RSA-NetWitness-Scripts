f35cd3a520f4f82eff5afac5563814a1aa98bc76
Aaron Martin <martina3203@gmail.com>
2020-04-20 17:49:47 -0400

    Created EndpointStats as well as updated other scripts.
    EndpointStats was created to be ran as cron job to record data about the endpoint server process.
    
    MoveToNewNodeZero.sh has been updated to include 4 launch services but with the caveat that these may not work. Initial testing seems positive.
    
    RabbitStats.sh has been updated to include full path names in places.

--- MoveToNewNodeZero.sh
+++ MoveToNewNodeZero.sh
@@ -29,7 +29,7 @@ yum clean all -q 2> /dev/null
 
 #Stop any relevant services
 serviceNames=("nwappliance" "nwlogcollector" "nwlogdecoder" "nwconcentrator" "nwbroker" "nwarchiver" "nwdecoder" "mongod" "rabbitmq-server" "rsa-nw-contexthub-server" "rsa-nw-correlation-server" "rsa-nw-esa-analytics-server")
-echo "Stopping services before going further. If this seems like it can be stuck for an excessisve amount of time, you may Ctrl + C and rerun the script after you manually stop them."
+echo "Stopping services before going further. If this seems like it can be stuck for an excessive amount of time, you may Ctrl + C and rerun the script after you manually stop them."
 for service in ${serviceNames[@]}; do
     if systemctl is-active --quiet $service ; then
         echo "Stopping $service... This may take some time."
@@ -38,7 +38,6 @@ for service in ${serviceNames[@]}; do
 done
 
 mv /etc/salt/pki/minion/minion_master.pub $DESTINATION_FOLDER/salt
-systemctl restart salt-minion
 
 #This section covers the directories that are common to all host.
 commonDirectories=("/etc/netwitness/platform" "/etc/netwitness/security-cli" "/etc/pki/nw")
@@ -65,7 +64,7 @@ done
 #This section covers the node infra service, if it exists. This was a change starting in the 11.4 release.
 infraServerDirectory="/etc/netwitness/node-infra-server"
 if [ -d $infraServerDirectory ]; then
-    echo "Moving $infraServerDirectory to $DESTINATION_FOLDER and removing rpm for clean discovery"
+    echo "Moving $infraServerDirectory to $DESTINATION_FOLDER"
     #yum remove rsa-nw-node-infra-server -y 2> /dev/null
     mv $infraServerDirectory $DESTINATION_FOLDER
     mv /etc/systemd/system/rsa-nw-node-infra-server.service.d/* $DESTINATION_FOLDER/systemd
@@ -73,48 +72,49 @@ if [ -d $infraServerDirectory ]; then
 fi
 
 #This section covers the launch services I have developed a method to deal with. Not all will be here, if any. This is still a work in progress but I'll leave it in here.
-#contexthubServerDirectory="/etc/netwitness/contexthub-server"
-#if [ -d $contexthubServerDirectory ]; then
-#    echo "Moving $contexthubServerDirectory to $DESTINATION_FOLDER and removing rpm for clean discovery"
-#    #yum remove  rsa-nw-contexthub-server -y 2> /dev/null
-#    mv $contexthubServerDirectory $DESTINATION_FOLDER
-#    mv /etc/systemd/system/rsa-nw-contexthub-server.service.d/* $DESTINATION_FOLDER/systemd
-#    #The purpose of these two lines is so that Chef knows not try and reconfigure an already configured mongo.
-#    mkdir -p /etc/netwitness/platform/mongo
-#    touch /etc/netwitness/platform/mongo/mongo.registered
-#    systemctl daemon-reload
-#fi
+contexthubServerDirectory="/etc/netwitness/contexthub-server"
+if [ -d $contexthubServerDirectory ]; then
+    echo "Moving $contexthubServerDirectory to $DESTINATION_FOLDER"
+    #yum remove  rsa-nw-contexthub-server -y 2> /dev/null
+    mv $contexthubServerDirectory $DESTINATION_FOLDER
+    mv /etc/systemd/system/rsa-nw-contexthub-server.service.d/* $DESTINATION_FOLDER/systemd
+    #The purpose of these two lines is so that Chef knows not try and reconfigure an already configured mongo.
+    mkdir -p /etc/netwitness/platform/mongo
+    touch /etc/netwitness/platform/mongo/mongo.registered
+    systemctl daemon-reload
+fi
 
-#correlationServerDirectory="/etc/netwitness/correlation-server"
-#if [ -d $correlationServerDirectory ]; then
-#    echo "Moving $correlationServerDirectory to $DESTINATION_FOLDER and removing rpm for clean discovery"
-#    #yum remove rsa-nw-node-infra-server -y 2> /dev/null
-#    mv $correlationServerDirectory $DESTINATION_FOLDER
-#    mv /etc/systemd/system/rsa-nw-correlation-server.service.d/* $DESTINATION_FOLDER/systemd
-#    systemctl daemon-reload
-#    yum reinstall rsa-nw-esper-enterprise -y -q 2> /dev/null
-#fi
+correlationServerDirectory="/etc/netwitness/correlation-server"
+if [ -d $correlationServerDirectory ]; then
+    echo "Moving $correlationServerDirectory to $DESTINATION_FOLDER"
+    #yum remove rsa-nw-node-infra-server -y 2> /dev/null
+    mv $correlationServerDirectory $DESTINATION_FOLDER
+    mv /etc/systemd/system/rsa-nw-correlation-server.service.d/* $DESTINATION_FOLDER/systemd
+    systemctl daemon-reload
+    yum reinstall rsa-nw-esper-enterprise -y -q 2> /dev/null
+fi
 
-#esaAnalyticsServerDirectory="/etc/netwitness/esa-analytics-server"
-#if [ -d $esaAnalyticsServerDirectory ]; then
-#   echo "Moving $esaAnalyticsServerDirectory to $DESTINATION_FOLDER and removing rpm for clean discovery"
-#   #yum remove rsa-nw-node-infra-server -y 2> /dev/null
-#   mv $esaAnalyticsServerDirectory $DESTINATION_FOLDER
-#   mv /etc/systemd/system/rsa-nw-esa-analytics-server.service.d/* $DESTINATION_FOLDER/systemd
-#   systemctl daemon-reload
-#fi
+esaAnalyticsServerDirectory="/etc/netwitness/esa-analytics-server"
+if [ -d $esaAnalyticsServerDirectory ]; then
+   echo "Moving $esaAnalyticsServerDirectory to $DESTINATION_FOLDER"
+   #yum remove rsa-nw-node-infra-server -y 2> /dev/null
+   mv $esaAnalyticsServerDirectory $DESTINATION_FOLDER
+   mv /etc/systemd/system/rsa-nw-esa-analytics-server.service.d/* $DESTINATION_FOLDER/systemd
+   systemctl daemon-reload
+fi
 
-#endpointServerDirectory="/etc/netwitness/endpoint"
-#if [ -d $endpointServerDirectory ]; then
-#   echo "Moving $endpointServerDirectory to $DESTINATION_FOLDER and removing rpm for clean discovery"
-#   #yum remove rsa-nw-endpoint-server -y 2> /dev/null
-#   mv $endpointServerDirectory $DESTINATION_FOLDER
-#   mv /etc/systemd/system/rsa-nw-endpoint-server.service.d/* $DESTINATION_FOLDER/systemd
-#   #The purpose of these two lines is so that Chef knows not try and reconfigure an already configured mongo.
-#   mkdir -p /etc/netwitness/platform/mongo
-#   touch /etc/netwitness/platform/mongo/mongo.registered
-#   systemctl daemon-reload
-#fi
+endpointServerDirectory="/etc/netwitness/endpoint-server"
+if [ -d $endpointServerDirectory ]; then
+   echo "Moving $endpointServerDirectory to $DESTINATION_FOLDER"
+   echo "Please note that for this specific service a redeployment of the agents may be expected."
+   #yum remove rsa-nw-endpoint-server -y 2> /dev/null
+   mv $endpointServerDirectory $DESTINATION_FOLDER
+   mv /etc/systemd/system/rsa-nw-endpoint-server.service.d/* $DESTINATION_FOLDER/systemd
+   #The purpose of these two lines is so that Chef knows not try and reconfigure an already configured mongo.
+   mkdir -p /etc/netwitness/platform/mongo
+   touch /etc/netwitness/platform/mongo/mongo.registered
+   systemctl daemon-reload
+fi
 
 echo "Reinstalling cookbooks and component-descriptor rpms to ensure that we can continue to reinstall. \
 If this step fails and nwsetup-tui complains about missing cookbooks, you may need to find a way to do this manaully."
--- RabbitStats.sh
+++ RabbitStats.sh
@@ -3,21 +3,21 @@
 #It will output it all ot a file in /tmp called rabbitstats
 DESTINATION_FOLDER="/tmp/rabbitstats"
 
-mkdir $DESTINATION_FOLDER
+mkdir -p $DESTINATION_FOLDER
 
 #Collecting information presented by the service
-rabbitmqctl status > $DESTINATION_FOLDER/rabbitmq-status.out
-rabbitmqctl report > $DESTINATION_FOLDER/rabbitmq-report.out
-rabbitmqctl list_queues -p /rsa/system name messages consumers > $DESTINATION_FOLDER/list_queues-rsa-system.out
+/usr/sbin/rabbitmqctl status > $DESTINATION_FOLDER/rabbitmq-status.out
+/usr/sbin/rabbitmqctl report > $DESTINATION_FOLDER/rabbitmq-report.out
+/usr/sbin/rabbitmqctl list_queues -p /rsa/system name messages consumers > $DESTINATION_FOLDER/list_queues-rsa-system.out
 #Let's say we start targetting a log collector
-rabbitmqctl list_queues -p logcollection name messages consumers > $DESTINATION_FOLDER/list_queues-logcollection.out
+/usr/sbin/rabbitmqctl list_queues -p logcollection name messages consumers > $DESTINATION_FOLDER/list_queues-logcollection.out
 
 #Netstat and process information
 netstat -anp > $DESTINATION_FOLDER/netstat.out
 ss -n > $DESTINATION_FOLDER/ss-n.out
 ps aux > $DESTINATION_FOLDER/ps-aux.out
-systemctl status rabbitmq-server -l > $DESTINATION_FOLDER/systemctl-status-rabbit-l.out
-systemctl status > $DESTINATION_FOLDER/systemctl-status.out 
+/usr/bin/systemctl status rabbitmq-server -l > $DESTINATION_FOLDER/systemctl-status-rabbit-l.out
+/usr/bin/systemctl status > $DESTINATION_FOLDER/systemctl-status.out 
 
 #lsof output
 lsof /var/netwitness/ > $DESTINATION_FOLDER/lsof-netwitnesshome.out
@@ -28,13 +28,13 @@ ulimit -a > $DESTINATION_FOLDER/ulimit-a.out
 cp /etc/security/limits.conf $DESTINATION_FOLDER/security-limits.conf
 
 #We talked about SMS being part of the problem so I am going to include it. Also, I am lazy and decided not to bother trying to grep it out of the next step.
-systemctl status rsa-sms -l > $DESTINATION_FOLDER/systemctl-status-rsa-sms-l.out
+/usr/bin/systemctl status rsa-sms -l > $DESTINATION_FOLDER/systemctl-status-rsa-sms-l.out
 
 #This will create a list that will allow us to iterate and look in the /proc folder for each item
 for pid in `ps aux | grep -i rabbitmq | grep -v "grep" | awk '{print $2}'| grep -i [0-9]`; 
     do 
     echo "checking /proc folder of $pid" 
-    mkdir $DESTINATION_FOLDER/$pid
+    mkdir -p $DESTINATION_FOLDER/$pid
     cat /proc/$pid/limits > $DESTINATION_FOLDER/$pid/limits.out
     ls -alh /proc/$pid/fd* > $DESTINATION_FOLDER/$pid/fdinfo-ls-alh.out
 done;
