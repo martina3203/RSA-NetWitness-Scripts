cdc778ff3322b826629f494bf1f0eddc1e1de52f
Aaron Martin <martina3203@gmail.com>
2020-08-17 10:16:52 -0400

    Heavy Refactoring into functions for better script management

--- MoveToNewNodeZero.sh
+++ MoveToNewNodeZero.sh
@@ -10,10 +10,14 @@
 #Please note that at this time, this script is only certified completely for core devices and your mileage may vary on other Launch devices like ESA and Endpoint.
 #Just because the service installs does not mean you are done. You should check that every aspect of that service is functioning as expected a result.
 
-echo "Please ensure you DO NOT run this on the Admin Server. If you have by mistake, please hit Ctrl + C now to break out now."
-echo "Please also ensure that the Admin Server you are moving to is of the same Major Release before proceeding. For example, 11.3.X.X to 11.3.X.X"
-read -p "Press enter to continue"
+#This is where we will backup our files
+DESTINATION_FOLDER="/tmp/PreviousNodeZeroFiles.`date +%Y-%b-%d-%H%M`"
+COMMON_DIRECTORIES=("/etc/netwitness/platform" "/etc/netwitness/security-cli" "/etc/pki/nw" "/etc/netwitness/orchestration-client")
+CORE_SERVICE_DIRECTORIES=("/etc/netwitness/ng/appliance" "/etc/netwitness/ng/logcollector" "/etc/netwitness/ng/logdecoder" "/etc/netwitness/ng/decoder" "/etc/netwitness/ng/broker" "/etc/netwitness/ng/archiver" "/etc/netwitness/ng/concentrator" "/etc/netwitness/ng/rabbitmq")
 
+
+#This function is reserved for cleaning the common launches services which generally have the same sort of steps that must be performed.
+#I have created a case statement for these services for any special criteria that may be associated with them.
 cleanLaunchService() {
     local launchDirectory=$1
     local serviceName=$2
@@ -26,88 +30,113 @@ cleanLaunchService() {
     #If we find special conditions for various launch services, we shall carry out those steps here.
     case $serviceName in
         "rsa-nw-endpoint-server")
-            echo -e "/e[93mPlease note this process is not guaranteed to work on Endpoint Servers.\e[39m"
-            echo "You should be expecting to redeploy the agents as a result." ;;
+            warning "Please note this process is not guaranteed to work on Endpoint Servers."
+            warning "You may be expected to redeploy the agents as a result." 
+            mkdir -p /etc/pki/nw/nwe-ca
+            #We are putting the Endpoint Root CA certificates that we removed in bulk back so that we can resume agent connection, hopefully.
+            cp $DESTINATION_FOLDER/nw/nwe-ca/nwerootca-cert.pem $DESTINATION_FOLDER/nw/nwe-ca/nwerootca-key.pem /etc/pki/nw/nwe-ca 
+            chown -R netwitness:nwpki /etc/pki/nw ;;
         "rsa-nw-correlation-server" | "rsa-nw-esa-server")
-            echo "Please note that you may need to update the Incident Counter on the Admin Server if this device will be the new ESA Primary and the previous INC counter is at a lower value."
-            echo "Please see the KB article where you found this script for more details on this process.";;
+            warning "Please note that you may need to update the Incident Counter on the Admin Server if this device will be the new ESA Primary and the previous INC counter is at a lower value."
+            warning "Please see the KB article where you found this script for more details on this process.";;
     esac
 }
 
-#sanity checks to make sure this is not an Admin Server, otherwise we are quitting.
-if grep "node-zero" /etc/netwitness/platform/nw-node-type; then 
-    echo -e "\e[91mScript detected this is not a valid host to run this script on after reviewing /etc/netwitness/platform/nw-node-type. Exiting...\e[39m"
-    exit 0
-fi
-if rpm -qa | grep "admin-server"; then
-    echo -e "\e[91mDetected Admin Server rpms on host. Please confirm this is not an Admin Server before attempting again. Exiting...\e[39m"
-    exit 0
-fi 
+#These functions are merely for my color coding messages
+exitError() {
+    echo -e "\e[91m$1\e[97m"
+    exit 1
+}
 
-#This is the destination folder for all files that we are simply moving out.
-DESTINATION_FOLDER="/tmp/PreviousNodeZeroFiles"
+warning() {
+    echo -e "\e[93m$1\e[97m"
+}
+
+#sanity checks to make sure this is not an Admin Server, otherwise we are quitting.
+checkNotNodeZero() {
+    warning "Please ensure you DO NOT run this on the Admin Server. If you have by mistake, please hit Ctrl + C now to break out now."
+    warning "Please also ensure that the Admin Server you are moving to is of the same Major Release before proceeding. For example, 11.3.X.X to 11.3.X.X"
+    read -p "Press enter to continue"
+    if grep "node-zero" /etc/netwitness/platform/nw-node-type; then 
+        exitError "Script detected this is not a valid host to run this script on after reviewing /etc/netwitness/platform/nw-node-type. Exiting..."
+    fi
+    if rpm -qa | grep "admin-server"; then
+        exitError "Detected Admin Server rpms on host. Please confirm this is not an Admin Server before attempting again. Exiting..."
+    fi 
+}
+
 echo "All files will be moved to $DESTINATION_FOLDER"
-mkdir -p $DESTINATION_FOLDER
 mkdir -p $DESTINATION_FOLDER/ng
 mkdir -p $DESTINATION_FOLDER/systemd
 
 #Forcing a clean of yum to make sure we get correct rpm information.
-yum clean all -q 2> /dev/null
 
-#We are checking for a mongo instance. We will be changing the password before proceeding.
-if systemctl list-units --all | grep -q mongod ; then
-    echo "A Mongo Instance has been detected on this device. Please provide the deployment password of the old environment so that we may login to mongo to change it to match the deployment password of the new environment."
-    echo "If you are unsure about the old password, please use KB 000037015 to reset the Mongo password in the backend then rerun this script."
-    echo "If the password is the same because it just is or you have already changed the password, just type the same password twice."
-    read -s -p 'Current Deployment password currently being used by the mongo: ' oldPassword
-    echo ""
-    read -s -p 'New Deployment password: ' newPassword
-    if mongo -u deploy_admin -p "${oldPassword}" --authenticationDatabase admin --eval "db=db.getSiblingDB(\"admin\");db.changeUserPassword(\"deploy_admin\",\"${newPassword}\")" --quiet; then
+#We are checking for a mongo instance. We will be changing the password before proceeding to prevent error
+checkMongo() {
+    if systemctl list-units --all | grep -q mongod ; then
+        echo "A Mongo Instance has been detected on this device. Please provide the deployment password of the old environment so that we may login to mongo to change it to match the deployment password of the new environment."
+        echo "If you are unsure about the old password, please use KB 000037015 to reset the Mongo password in the backend then rerun this script."
+        echo "If the password is the same because it just is or you have already changed the password, just type the same password twice."
+        read -s -p 'Current Deployment password currently being used by the mongo: ' oldPassword
+        echo ""
+        read -s -p 'New Deployment password: ' newPassword
+        mongo -u deploy_admin -p "${oldPassword}" --authenticationDatabase admin --eval "db=db.getSiblingDB(\"admin\");db.changeUserPassword(\"deploy_admin\",\"${newPassword}\")" || 
+        exitError "Unable to change current mongodb password. Please confirm you have the correct password or follow KB article https://community.rsa.com/docs/DOC-100186 to change it in the backend for this host. Then, you may type the same password again twice for old and new."
+        #Create the corresponding marker files
         mkdir -p /etc/netwitness/platform/mongo
         touch /etc/netwitness/platform/mongo/mongo.registered
-        echo -e "\e[93mPlease be sure that the empty file /etc/netwitness/platform/mongo/mongo.registered is created as a result of this command.\e[39m"
-    else 
-        echo -e "\e[91mUnable to change current mongodb password. Please confirm you have the correct password or follow KB article https://community.rsa.com/docs/DOC-100186 to change it in the backend for this host. Then, you may type the same password again twice for old and new.\e[39m"
-        exit 1
     fi
-fi
+}
 
 #Stop any relevant services
-serviceNames=("salt-minion" "nwappliance" "nwlogcollector" "nwlogdecoder" "nwconcentrator" "nwbroker" "nwarchiver" "nwworkbench" "nwdecoder" "mongod" "rabbitmq-server" "rsa-nw-contexthub-server" "rsa-nw-correlation-server" "rsa-nw-esa-analytics-server" "rsa-nw-node-infra-server")
-echo "Stopping services before going further. If this seems like it can be stuck for an excessive amount of time, you may Ctrl + C and rerun the script after you manually stop them."
-for service in ${serviceNames[@]}; do
-    if systemctl is-active --quiet $service ; then
-        echo "Stopping $service... This may take some time."
-        systemctl stop $service
-    fi
-done
+stopServices() {
+    serviceNames=("salt-minion" "nginx" "collectd" "nwappliance" "nwlogcollector" "nwlogdecoder" "nwconcentrator" "nwbroker" "nwarchiver" "nwworkbench" "nwdecoder" "mongod" "rabbitmq-server" "rsa-nw-contexthub-server" "rsa-nw-correlation-server" "rsa-nw-esa-analytics-server" "rsa-nw-node-infra-server")
+    echo "Stopping services before going further. If this seems like it can be stuck for an excessive amount of time, you may Ctrl + C and rerun the script after you manually stop them."
+    for service in ${serviceNames[@]}; do
+        if systemctl is-active --quiet $service ; then
+            echo "Stopping $service... This may take some time."
+            systemctl stop $service
+        fi
+    done
+}
 
+backupDirectory() {
+#This section covers the directories that are common to all host.
+    if [ -d $1 ]; then
+        echo "Moving $1 to $2"
+        mv -f $1 $2
+    else
+        echo "$directory not detected. Skipping..."
+    fi
+}
+
+########################################################################
+#                     MAIN
+########################################################################
+
+checkNotNodeZero
+checkMongo
 #This resets the salt master public key that is stored on the device.
 mv -f /etc/salt/pki/minion/minion_master.pub $DESTINATION_FOLDER/salt
-
-#This section covers the directories that are common to all host.
-commonDirectories=("/etc/netwitness/platform" "/etc/netwitness/security-cli" "/etc/pki/nw" "/etc/netwitness/orchestration-client")
-for directory in ${commonDirectories[@]}; do
+#Let's not have a bad yum cache
+yum clean all -q 2> /dev/null
+warning "All files will be moved to $DESTINATION_FOLDER"
+mkdir -p $DESTINATION_FOLDER/ng
+mkdir -p $DESTINATION_FOLDER/systemd
+#These are our common directories that all hosts pretty much share.
+for directory in ${COMMON_DIRECTORIES[@]}; do
     if [ -d $directory ]; then
-        echo "Moving $directory to $DESTINATION_FOLDER"
-        mv -f $directory $DESTINATION_FOLDER
-    else
-        echo "$directory not detected. Skipping..."
+        backupDirectory $directory $DESTINATION_FOLDER
+    fi
+done
+#These are targeted towards the Core/C++ services
+for directory in ${CORE_SERVICE_DIRECTORIES[@]}; do
+    if [ -d $directory ]; then
+        backupDirectory $directory $DESTINATION_FOLDER/ng
     fi
 done
 
-#The following section covers the truststores of the Core services
-coreServiceDirectoryList=("/etc/netwitness/ng/appliance" "/etc/netwitness/ng/logcollector" "/etc/netwitness/ng/logdecoder" "/etc/netwitness/ng/decoder" "/etc/netwitness/ng/broker" "/etc/netwitness/ng/archiver" "/etc/netwitness/ng/concentrator")
-for directory in ${coreServiceDirectoryList[@]}; do
-    if [ -d $directory ]; then
-        echo "Moving $directory to $DESTINATION_FOLDER"
-        mv -f $directory $DESTINATION_FOLDER/ng
-    else
-        echo "$directory not detected. Skipping..."
-    fi
-done
-
-#The below seciton cleans up the launch entries and responds accordingly
+#The below seciton cleans up the launch/Java entries and responds accordingly based on criteria that may or may not apply to it.
 cleanLaunchService /etc/netwitness/node-infra-server rsa-nw-node-infra-server
 cleanLaunchService /etc/netwitness/contexthub-server rsa-nw-contexthub-server
 cleanLaunchService /etc/netwitness/correlation-server rsa-nw-correlation-server
@@ -116,8 +145,8 @@ cleanLaunchService /etc/netwitness/esa-server rsa-nw-esa-server
 cleanLaunchService /etc/netwitness/endpoint-server rsa-nw-endpoint-server
 systemctl daemon-reload
 
-echo "The script execution is now complete. Please verify the following items before re-running nwsetup-tui in Install Mode:"
-echo "Please ensure that the version of this device has a matching repo on the Admin Server/Web Repo. If not, please add one."
-echo "Please remove any repo files in /etc/yum.repos.d that are pointing to older versions of NetWitness that are no longer relevant, such as 11.1.0.1."
-echo "Please note that if you have ever had to make workarounds in the chef recipes, you will need to reapply them accordingly."
+echo "The script execution is now complete. Please verify the following items before re-running nwsetup-tui in Install Mode."
+warning "Please ensure that the version of this device has a matching repo on the Admin Server/Web Repo. If not, please add one by following the relevant upgrade guide to get the repo initialized."
+warning "Please remove any repo files in /etc/yum.repos.d that are pointing to older versions of NetWitness that are no longer relevant, such as 11.1.0.1."
+warning "Please note that if you have ever had to make workarounds in the chef recipes, you will need to reapply them accordingly."
 echo "The backing up and moving of files is now complete. Please rerun nwsetup-tui to discover the host on the new Admin Node."
